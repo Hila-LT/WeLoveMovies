17f74409a9273dba02bc2dd400a8def0cfc318a6
Hilla Landau Tahery <htahary@indeed.com>
2024-08-31 22:09:44 +0900

    creating the tables migrations

---
+++ src/db/migrations/20240831123508_createMovies.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+/*
+movie_id: (Primary Key) A unique ID for the movie.
+title: (String) The title of the movie.
+runtime_in_minutes: (Integer) The length of the movie in minutes.
+rating: (String) The rating given to the movie.
+description: (Text) A shortened description of the movie.
+image_url: (String) A URL to the movie's poster.
+ */
+
+exports.up = function(knex) {
+    return knex.schema.createTable("movies", (table) => {
+        table.increments("movie_id").primary();
+        table.string("title");
+        table.integer("runtime_in_minutes")
+        table.enum("rating", ["G", "PG", "PG-13", "R", "NC-17", "NR"]);
+        table.text("description");
+        table.string("image_url");
+        table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("movies");
+};
---
+++ src/db/migrations/20240831124702_createMovies-Theaters.js
@@ -0,0 +1,40 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+/*movie_id: (Foreign Key) A reference ID to a particular movie.
+theater_id: (Foreign Key) A reference ID to a particular theater.
+is_showing: (Boolean) A representation of whether or not the movie is currently showing in the referenced theater.
+*/
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = function(knex) {
+    return knex.schema.createTable("movies-theaters", (table) => {
+        table.boolean("is_showing").defaultTo(false);
+        table.integer("movie_id").unsigned().notNullable();
+        table
+            .foreign("movie_id")
+            .references("movie_id")
+            .inTable("movies")
+            .onDelete("cascade");
+        table.integer("theater_id").unsigned().notNullable();
+        table
+            .foreign("theater_id")
+            .references("theater_id")
+            .inTable("theaters")
+            .onDelete("cascade");
+        table.timestamps(true, true);
+
+
+    });
+};
+
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("movies_theaters");
+};
